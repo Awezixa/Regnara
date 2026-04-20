e091e2658e8c3f37eedbe002fddb6a81391a034a
anis3-arch <derardjaa93@gmail.com>
2026-04-20 14:21:31 +0100

    techTree.h

--- tech_Tree/techTree.h
+++ tech_Tree/techTree.h
@@ -1,5 +1,21 @@
 #ifndef TECH_TREES_H
 #define TECH_TREES_H
-#include "../game.h"
+
+#define MAX_UPGRADES 6
+
+typedef struct {
+    char name[50];
+    int cost;
+    int unlocked;
+    int required;
+} Upgrade;
+
+typedef struct {
+    Upgrade upgrades[MAX_UPGRADES];
+} TechTree;
+
+void initTechTree(TechTree *tree);
+void unlockUpgrade(TechTree *tree, int index, int *gold);
+void printTechTree(TechTree *tree);
 
 #endif
